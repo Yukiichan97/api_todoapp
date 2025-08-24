422a7dd8c93323fda0a83e63dc55a39569b41d7f
HoaiMinh <quangluatlm@gmail.com>
2025-08-24 22:33:59 +0700

    Add CORS

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,12 +2,22 @@ package server
 
 import (
 	"backend_todoapp/internal/tasks"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewServer() *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.CORS())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+	}))
+
 	repo := tasks.NewRepository()
 	service := tasks.NewService(repo)
 	handler := tasks.NewHandler(service)
